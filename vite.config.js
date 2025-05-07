02006362e6a5885dd5e8e91c41c775c899f428cd
tortangChris <eliseriochrisashley@gmail.com>
2025-05-07 21:53:51 +0800

    Add EduAR Icon and WebTitle

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -33,18 +32,18 @@ export default defineConfig({
         short_name: "EduAR",
         description: "Educational AR",
         theme_color: "#ffffff",
-        // icons: [
-        //   {
-        //     src: "pwa-192x192.png",
-        //     sizes: "192x192",
-        //     type: "image/png",
-        //   },
-        //   {
-        //     src: "pwa-512x512.png",
-        //     sizes: "512x512",
-        //     type: "image/png",
-        //   },
-        // ],
+        icons: [
+          {
+            src: "icons/EduAr.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/EduAr.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
     }),
   ],
