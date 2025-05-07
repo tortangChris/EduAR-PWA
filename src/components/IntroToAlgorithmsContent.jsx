cb06eb0ee9d9fb8d4e12237bd8be2c48e0ead0cf
tortangChris <eliseriochrisashley@gmail.com>
2025-05-08 00:54:55 +0800

    moved finish button

--- src/components/IntroToAlgorithmsContent.jsx
+++ src/components/IntroToAlgorithmsContent.jsx
@@ -1,29 +1,11 @@
-import { CheckCircle } from "lucide-react";
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
 import content from "../../public/markdown/1.md?raw";
 
 const IntroToAlgorithmsContent = () => {
-  const navigate = useNavigate();
-
-  const handleFinishModule = () => {
-    navigate("/modules");
-  };
-
   return (
     <div className="bg-base-200 rounded-xl shadow-md h-[calc(86vh-6.5rem)] overflow-y-auto p-6 space-y-4 text-left">
       <ReactMarkdown>{content}</ReactMarkdown>
-
-      <div className="flex justify-end pt-4">
-        <button
-          onClick={handleFinishModule}
-          className="btn btn-success flex items-center gap-2"
-        >
-          <CheckCircle className="w-5 h-5" />
-          Finish Module
-        </button>
-      </div>
     </div>
   );
 };
--- src/modules/IntroductionToAlgorithms.jsx
+++ src/modules/IntroductionToAlgorithms.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import IntroToAlgorithmsHeader from "../components/IntroToAlgorithmsHeader";
 import IntroToAlgorithmsContent from "../components/IntroToAlgorithmsContent";
+import { CheckCircle } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const IntroductionToAlgorithms = () => {
+  const navigate = useNavigate();
+
+  const handleFinishModule = () => {
+    navigate("/modules");
+  };
   return (
-    <div className="h-[calc(100vh-4rem)] overflow-y-auto p-4 bg-base-100 space-y-4">
+    <div className="h-[calc(100vh)] overflow-y-auto p-4 bg-base-100 space-y-4">
       <IntroToAlgorithmsHeader />
       <IntroToAlgorithmsContent />
+      <div className="flex justify-end">
+        <button
+          onClick={handleFinishModule}
+          className="btn btn-success flex items-center gap-2"
+        >
+          <CheckCircle className="w-5 h-5" />
+          Finish Module
+        </button>
+      </div>
     </div>
   );
 };
