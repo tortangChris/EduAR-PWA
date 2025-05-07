bd38af246894e4c07d317204d35c61f6c2d513f2
tortangChris <eliseriochrisashley@gmail.com>
2025-05-08 05:44:35 +0800

    add module content to Advanced Data Structure

--- src/components/AdvancedDataStructuresContent.jsx
+++ src/components/AdvancedDataStructuresContent.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
-// import content from "../../public/markdown/1.md?raw";
+import content from "../../public/markdown/5.md?raw";
+import remarkGfm from "remark-gfm";
 
 const AdvancedDataStructuresContent = () => {
   return (
     <div className="bg-base-200 rounded-xl shadow-md h-[calc(86vh-6.5rem)] overflow-y-auto p-6 space-y-4 text-left">
-      {/* <div className="markdown-body">
-        <ReactMarkdown>{content}</ReactMarkdown>
-      </div> */}
+      <div className="markdown-body">
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+      </div>
     </div>
   );
 };
