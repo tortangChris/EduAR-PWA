f91760877dc852b293c06911aefae846b3e3e280
tortangChris <eliseriochrisashley@gmail.com>
2025-05-08 01:33:05 +0800

    Fix md file Formatting

--- src/components/IntroToAlgorithmsContent.jsx
+++ src/components/IntroToAlgorithmsContent.jsx
@@ -5,7 +5,9 @@ import content from "../../public/markdown/1.md?raw";
 const IntroToAlgorithmsContent = () => {
   return (
     <div className="bg-base-200 rounded-xl shadow-md h-[calc(86vh-6.5rem)] overflow-y-auto p-6 space-y-4 text-left">
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <div className="markdown-body">
+        <ReactMarkdown>{content}</ReactMarkdown>
+      </div>
     </div>
   );
 };
