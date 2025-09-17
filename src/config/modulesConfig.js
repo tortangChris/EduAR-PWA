4485f3a492e505c8ffc5aba54aee99c05f26303b
tortangChris <eliseriochrisashley@gmail.com>
2025-09-18 00:12:49 +0800

    Change Module Thumbnails

--- src/config/modulesConfig.js
+++ src/config/modulesConfig.js
@@ -2,47 +2,47 @@ const modulesConfig = [
   {
     title: "Arrays & Time Complexity",
     route: "/modules/arrays",
-    image: "/images/Array.png",
+    image: "/images/Array_and_TimeComplexity.png",
   },
   {
     title: "Sorting Algorithms",
     route: "/modules/sorting",
-    image: "/images/Sorting Algorith.png",
+    image: "/images/Sorting_Algorithm.png",
   },
   {
     title: "Dynamic Arrays and Multi-Dimensional Arrays",
     route: "/modules/dynamic-and-multi-dimensional-arrays",
-    image: "/images/Sorting Algorith.png",
+    image: "/images/Dynamic_and_MultiDimensional.png",
   },
   {
     title: "Linked List Varation",
     route: "/modules/linked-list",
-    image: "/images/Linked List.png",
+    image: "/images/LinkedList_Variation.png",
   },
   {
     title: "Stack and Queue",
     route: "/modules/stack-and-queue",
-    image: "/images/arraylogo.jpg",
+    image: "/images/Stack&Queue.png",
   },
   {
     title: "Tree Data Structure Recursion",
     route: "/modules/tree-data-structure-recursion",
-    image: "/images/Trees.png",
+    image: "/images/TreeRecursion.png",
   },
   {
     title: "Set Data Structure and Operations",
     route: "/modules/set-data-structure",
-    image: "/images/set.jpg",
+    image: "/images/SetDataStructure.png",
   },
   {
     title: "Graph Data Structure and Operations",
     route: "/modules/graph-data-structure",
-    image: "/images/graphlogo.jpg",
+    image: "/images/GraphDataStructure.png",
   },
   {
     title: "Map & Hash Table",
     route: "/modules/map-and-hash-table",
-    image: "/images/graphlogo.jpg",
+    image: "/images/Map&HashTable.png",
   },
 ];
 
