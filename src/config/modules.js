8eb8f8b7467048553b40497676fe3b21da48d416
tortangChris <eliseriochrisashley@gmail.com>
2025-09-18 21:57:38 +0800

    Reduce Module Contents

--- src/config/modules.js
+++ src/config/modules.js
@@ -1,14 +1,14 @@
 const modules = {
   arrays: "Arrays",
   sorting: "Sorting",
-  "dynamic-and-multi-dimensional-arrays":
-    "Dynamic and Multi-dimensional arrays",
+  // "dynamic-and-multi-dimensional-arrays":
+  //   "Dynamic and Multi-dimensional arrays",
   "linked-list": "Linked List Variation",
   "stack-and-queue": "Stack and Queue",
-  "tree-data-structure-recursion": "Tree Recursion",
-  "set-data-structure": "Set Data Structure",
+  "tree-data-structure-recursion": "Tree Data Structure",
+  // "set-data-structure": "Set Data Structure",
   "graph-data-structure": "Graph Data Structure",
-  "map-and-hash-table": "Map and Hash Table",
+  // "map-and-hash-table": "Map and Hash Table",
 };
 
 export default modules;
--- src/config/modulesConfig.js
+++ src/config/modulesConfig.js
@@ -9,11 +9,11 @@ const modulesConfig = [
     route: "/modules/sorting",
     image: "/images/Sorting_Algorithm.png",
   },
-  {
-    title: "Dynamic Arrays and Multi-Dimensional Arrays",
-    route: "/modules/dynamic-and-multi-dimensional-arrays",
-    image: "/images/Dynamic_and_MultiDimensional.png",
-  },
+  // {
+  //   title: "Dynamic Arrays and Multi-Dimensional Arrays",
+  //   route: "/modules/dynamic-and-multi-dimensional-arrays",
+  //   image: "/images/Dynamic_and_MultiDimensional.png",
+  // },
   {
     title: "Linked List Varation",
     route: "/modules/linked-list",
@@ -29,21 +29,21 @@ const modulesConfig = [
     route: "/modules/tree-data-structure-recursion",
     image: "/images/TreeRecursion.png",
   },
-  {
-    title: "Set Data Structure and Operations",
-    route: "/modules/set-data-structure",
-    image: "/images/SetDataStructure.png",
-  },
+  // {
+  //   title: "Set Data Structure and Operations",
+  //   route: "/modules/set-data-structure",
+  //   image: "/images/SetDataStructure.png",
+  // },
   {
     title: "Graph Data Structure and Operations",
     route: "/modules/graph-data-structure",
     image: "/images/GraphDataStructure.png",
   },
-  {
-    title: "Map & Hash Table",
-    route: "/modules/map-and-hash-table",
-    image: "/images/Map&HashTable.png",
-  },
+  // {
+  //   title: "Map & Hash Table",
+  //   route: "/modules/map-and-hash-table",
+  //   image: "/images/Map&HashTable.png",
+  // },
 ];
 
 export default modulesConfig;
