2a25aa3967d67c0547e34d6713372abd14fd21f1
tortangChris <eliseriochrisashley@gmail.com>
2025-10-19 20:46:45 +0800

    fix bug

--- src/components/Sorting/Page1.jsx
+++ src/components/Sorting/Page1.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import VisualPage1 from "./VisualPage1";
 import ARButton from "./ARButton";
-import ArrayPage01 from "./Contents/SortingPage01";
+import SortingPage01 from "./Contents/SortingPage01";
 
 const Page1 = () => {
   return (
     <div className="bg-base-200 rounded-xl shadow-md h-[calc(82vh-6.5rem)] overflow-y-auto p-6 space-y-4 text-left">
       <ARButton />
 
-      <ArrayPage01 />
+      <SortingPage01 />
 
       <div className="w-full h-[300px] bg-gray-900 rounded-xl flex items-center justify-center relative">
         <VisualPage1 />
