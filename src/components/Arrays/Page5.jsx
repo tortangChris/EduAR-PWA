684ed113f129042aaf8605cc8db298d1526e2cd0
Sherwin727 <corralessherwin628@gmail.com>
2025-11-11 21:16:35 +0800

    (Array) Adjusted Page5

--- src/components/Arrays/Page5.jsx
+++ src/components/Arrays/Page5.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import VisualPage5 from "./VisualPage5";
-import ARButton from "./ARButton";
 import ArrayPage05 from "./Contents/ArrayPage05";
+import ARButton5 from "./ARButton5";
 
 const Page5 = () => {
   return (
     <div className="bg-base-200 rounded-xl shadow-md h-[calc(82vh-6.5rem)] overflow-y-auto p-6 space-y-4 text-left">
-      <ARButton />
+      <ARButton5 />
 
       <ArrayPage05 />
 
