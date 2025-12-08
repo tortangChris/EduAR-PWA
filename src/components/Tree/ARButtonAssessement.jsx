6d8f63c34f9ed2f2e0914e265c116740c73c8ddc
Jermel S Hernandez <jermelxbishi2@gmail.com>
2025-12-09 00:16:18 +0800

    Update ARButtonAssessement.jsx

--- src/components/Tree/ARButtonAssessement.jsx
+++ src/components/Tree/ARButtonAssessement.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Lock, Unlock } from "lucide-react";
-import StackQueueAssessmentAR from "./StackQueueAssessmentAR";
-import TreeAssessmentAR from "./TreeAssessmentAR";
+import LinkedListAssessmentAR from "./TreeAssessmentAR";
 
 const ARButtonAssessment = () => {
   const [isSupported, setIsSupported] = useState(null);
