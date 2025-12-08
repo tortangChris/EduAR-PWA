1f810f779e549258e3f03215f2e1c0153a6e21a2
tortangChris <eliseriochrisashley@gmail.com>
2025-12-08 23:00:52 +0800

    (Stack) Button Update

--- src/components/StackQueue/ARButtonAssessement.jsx
+++ src/components/StackQueue/ARButtonAssessement.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Lock, Unlock } from "lucide-react";
-import ARPage1 from "./ARPage1";
+import StackQueueAssessmentAr from "./StackQueueAssessmentAr";
 
 const ARButtonAssessment = () => {
   const [isSupported, setIsSupported] = useState(null);
@@ -26,7 +26,7 @@ const ARButtonAssessment = () => {
 
   // kung nag-start na si AR → render ARPage1
   if (startAR) {
-    return <ARPage1 />;
+    return <StackQueueAssessmentAr />;
   }
 
   if (isSupported === null) {
