d8ec36c96445610ae8c78a4c6a9724051f866273
tortangChris <eliseriochrisashley12@gmail.com>
2026-04-07 11:42:15 +0800

    try

--- src/components/Arrays/Page0.jsx
+++ src/components/Arrays/Page0.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ArrayPage0 from "./Contents/ArrayPage0";
+import ARButton from "./ARBtn";
 
 const Page0 = () => {
   return (
     <div className="bg-base-200 rounded-xl shadow-md h-[calc(86vh-6.5rem)] overflow-y-auto p-6 space-y-4 text-left">
+      <ARButton />
+
       <ArrayPage0 />
     </div>
   );
